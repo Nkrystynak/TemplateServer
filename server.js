bb393a596bfc55c679a4fd645f0092c0a9c25d0e
Nic Krystynak <Bet.arson.moon.guru565@protonmail.com>
2017-12-21 14:05:45 -0800

    updated 2 html files and one server file

--- server.js
+++ server.js
@@ -7,24 +7,38 @@ var path = require('path');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(request, response, nextMiddleware){
-  console.log("Somebody came into our website. This middleware activates no matter what.");
-  nextMiddleware();
-});
+people_visited = {
 
-var users = []
-
+};
 app.get('/', function(request, response){
   // request comes from the CLIENT. This is an object that contains details about what they're trying to do, or information you've logged on them.
   console.log("Somebody came to my website! They visited the root directory.");
   console.log(request.ip);
-  response.sendFile(__dirname + '/index.html');
+  if (people_visited[request.ip]) {
+    people_visited[request.ip] += 1;
+  } else{
+    people_visited[request.ip] = 1;
+  }
+
+  if(people_visited[request.ip] < 100){
+    response.sendFile(__dirname + '/index.html');
+  } else {
+    response.send("Stop visiting my website. You've already been here ${people_visited[request.ip]} times")
+  }
+
 });
+
 app.get('/elliot', function(request, response){
   // request comes from the CLIENT. This is an object that contains details about what they're trying to do, or information you've logged on them.
   console.log("Somebody came to my website! They visited the root directory.");
   console.log(request.ip);
   response.sendFile(__dirname + '/elliot.html');
+});
+app.get('/homework', function(request, response){
+  // request comes from the CLIENT. This is an object that contains details about what they're trying to do, or information you've logged on them.
+  console.log("Somebody came to my website! They visited the root directory.");
+  console.log(request.ip);
+  response.sendFile(__dirname + '/homework.html');
 });
 
 app.get('/club', function(request, response){
